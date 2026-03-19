689f4b8ccdb389e2215d499b39a4e137ea7899fa
Aaric Putt <176777119+AaricP@users.noreply.github.com>
2026-03-19 07:47:02 -0600

    web analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import "./globals.css";
+import { Analytics } from "@vercel/analytics/next";
   
 export const metadata: Metadata = {
   title: "Aaric Putt",
@@ -29,6 +30,7 @@ export default function RootLayout({
     <html lang="en">
       <body>
         {children}
+        <Analytics />
       </body>
     </html>
   );
